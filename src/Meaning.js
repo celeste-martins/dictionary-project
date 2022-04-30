47a3e1dc29d048c43936f56109f85963a5e0d7a8
celeste-martins <celeste.martins@gmail.com>
2022-04-30 09:42:38 -0300

    Styling dictionary

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Meaning.css";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
@@ -9,13 +10,9 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={props.meaning.synonyms} />
-            </p>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={props.meaning.synonyms} />
           </div>
         );
       })}
